f7897155568af7349b0c258740dbefe7b981d825
keil <38099918+zzzkeil@users.noreply.github.com>
2019-01-22 11:52:46 +0100

    Update w_d_iptables_setup.sh

--- untested/w_d_iptables_setup.sh
+++ untested/w_d_iptables_setup.sh
@@ -303,7 +303,7 @@ chmod +x /etc/dnscrypt-proxy/utils/generate-domains-blacklists/generate-domains-
 cd /etc/dnscrypt-proxy/utils/generate-domains-blacklists/
 ./generate-domains-blacklist.py > /etc/dnscrypt-proxy/blacklist.txt
 cd
-echo "51 11 * * 1,3,5 cd /etc/dnscrypt-proxy/utils/generate-domains-blacklists/ && /usr/bin/python generate-domains-blacklist.py > /etc/dnscrypt-proxy/blacklist.txt" >> blacklistcron
+echo "05 12 * * * cd /etc/dnscrypt-proxy/utils/generate-domains-blacklists/ && /usr/bin/python generate-domains-blacklist.py > /etc/dnscrypt-proxy/blacklist.txt" >> blacklistcron
 crontab blacklistcron
 rm blacklistcron
 
