755124df52fd09b4389566c5093c5a7072160188
keil <38099918+zzzkeil@users.noreply.github.com>
2019-01-23 21:28:18 +0100

    Update untested_wireguard_from_Source.sh

--- untested/untested_wireguard_from_Source.sh
+++ untested/untested_wireguard_from_Source.sh
@@ -75,9 +75,11 @@ apt install libmnl-dev libelf-dev build-essential pkg-config linux-headers-$(una
 mkdir -p /root/wireguard/src
 cd /root/wireguard/src
 wget https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20190123.tar.xz
-tar -xvzf WireGuard-0.0.20190123.tar.xz
+tar -xvf WireGuard-0.0.20190123.tar.xz
+cd /root/wireguard/src/WireGuard-0.0.20190123/src
 make
 make install
+cd
 
 ####
 clear
