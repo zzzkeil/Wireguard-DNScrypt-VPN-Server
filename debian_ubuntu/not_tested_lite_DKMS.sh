c94855939dadfd81f17aa0e771504e7da0886f9d
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-23 19:32:36 +0100

    Update not_tested_lite_DKMS.sh

--- debian_ubuntu/not_tested_lite_DKMS.sh
+++ debian_ubuntu/not_tested_lite_DKMS.sh
@@ -50,6 +50,8 @@ echo
 	 
 echo "Step 02 - Systemupdate and Downloads" 
 echo
+echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
 apt update && apt upgrade -y && apt autoremove -y
 apt update
 apt install qrencode unbound unbound-host python curl -y 
