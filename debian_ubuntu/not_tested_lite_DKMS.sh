5ae142c5b62665a31c84a7facd1e3c0a39218424
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-24 20:02:58 +0100

    Update not_tested_lite_DKMS.sh

--- debian_ubuntu/not_tested_lite_DKMS.sh
+++ debian_ubuntu/not_tested_lite_DKMS.sh
@@ -67,7 +67,7 @@ if [[ -e /etc/debian_version ]]; then
     if [[ "$VERSION_ID" != 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="10"' ]]; then
 	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
-    else
+    fi
     if [[ "$VERSION_ID" != 'VERSION_ID="18.04"' ]]; then
     add-apt-repository ppa:wireguard/wireguard
     fi
