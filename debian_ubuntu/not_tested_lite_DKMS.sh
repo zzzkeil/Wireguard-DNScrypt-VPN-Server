2a5ac4a00b9a066e0cd72eadd19786b5295b1389
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-25 18:32:54 +0100

    Update not_tested_lite_DKMS.sh

--- debian_ubuntu/not_tested_lite_DKMS.sh
+++ debian_ubuntu/not_tested_lite_DKMS.sh
@@ -21,11 +21,13 @@ fi
 if [[ -e /etc/debian_version ]]; then
     VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
     if [[ "$VERSION_ID" = 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="10"' ]] && [[ "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
-	echo "OS supported"
+	    echo "OS supported"
+      else
+      echo "Sorry, your Debian/Ubuntu Version is not supported. Only Debian 9 - 10 or Ubuntu 18.04"
+      exit 1
       fi
       else
-      echo "Sorry, your OS is not supported. Only Debian 9 - 10 or Ubuntu 18.04"
-      exit 1
+      echo "This is not a Debian Distribution. Only Debian 9 - 10 or Ubuntu 18.04 supported"
 fi
 
 
