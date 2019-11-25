a52f4bb127e8caf78152ea4a50e82b22e736e17f
keil <38099918+zzzkeil@users.noreply.github.com>
2019-11-25 20:13:26 +0100

    Update not_tested_lite_DKMS.sh

--- debian_ubuntu/not_tested_lite_DKMS.sh
+++ debian_ubuntu/not_tested_lite_DKMS.sh
@@ -59,6 +59,7 @@ echo
 	 
 echo "Step 02 - Systemupdate and Downloads" 
 echo
+VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
 if [[ "$VERSION_ID" = 'VERSION_ID="10"' ]]; then
 	echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable-wireguard.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
