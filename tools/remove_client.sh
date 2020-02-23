7189d678727f27cf6db2da95677bff948e1b23dc
keil <38099918+zzzkeil@users.noreply.github.com>
2020-02-23 16:11:12 +0100

    Create remove_client.sh

---
+++ tools/remove_client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo " Just 4 testing now "
+echo " maybe dont work "
+echo " be carefull"
+echo "."
+echo "."
+echo "."
+echo "."
+###
+echo "Client Name to remove"
+echo "Choose the # clientname above the line [Peer] in wg0.conf"
+read -p "client name: " -e -i removeclient clientname
+echo "------"
+
+rm /etc/wireguard/keys/$clientname
+rm /etc/wireguard/keys/$clientname.pub
+rm /etc/wireguard/$clientname.conf
+rm /etc/wireguard/$clientname.png
+sed -i "/# $clientname/,+3 d" /etc/wireguard/wg0.conf
+systemctl restart wg-quick@wg0.service
+echo "Client $clientname removed, wireguard restarted"
