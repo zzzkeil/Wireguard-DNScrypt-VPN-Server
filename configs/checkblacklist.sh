4973fae19d9e39310f74d8ebda5660144f5bc488
Klaus <38099918+zzzkeil@users.noreply.github.com>
2020-12-28 09:08:13 +0100

    Update and rename checkblacklist.sh to checkblocklist.sh

--- configs/checkblacklist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ -s /etc/dnscrypt-proxy/blacklist.txt ]]; then
-exit 0
-fi
-cd /etc/dnscrypt-proxy/utils/generate-domains-blacklists/ &&  ./generate-domains-blacklist.py > /etc/dnscrypt-proxy/blacklist.txt
-exit 0
