710928a5487b111dd31f46c776c58cf1d2fdc421
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-07-17 12:04:22 +0200

    Create backup_wg_config.sh

---
+++ tools/backup_wg_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo " this script backup yours wireguard server config "
+echo " in case if you need to reinstall your server, but not want to reconfigure your all clients"
+echo " "
+echo " work in progess "
+echo "."
+echo "."
+echo "."
