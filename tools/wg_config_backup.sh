82f55729a6c621f96e73fab561787b78e4cd4e62
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-07-17 19:12:17 +0200

    Update wg_config_backup.sh

--- tools/wg_config_backup.sh
+++ tools/wg_config_backup.sh
@@ -2,8 +2,6 @@
 clear
 echo " this script backup yours wireguard server config "
 echo " in case if you need to reinstall your server, but not want to reconfigure your all clients"
-echo " "
-echo " work in progess "
 echo "."
 echo "."
 echo "."
@@ -11,6 +9,6 @@ tar -czf  /root/backup_wg_config.tar.gz /etc/wireguard
 echo "."
 echo "."
 echo "."
+echo " done backupfile is /root/backup_wg_config.tar.gz "
 echo " download / copy / save the file to your needs "
-echo " to restore run ./wg_config_restore.sh "
-
+echo " to restore place the file here /root/backup_wg_config.tar.gz and run ./wg_config_restore.sh "
