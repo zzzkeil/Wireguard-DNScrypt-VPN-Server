77ac68aebdcb658be0942ebcb5a17d974d6c2215
keil <38099918+zzzkeil@users.noreply.github.com>
2023-08-13 19:39:53 +0200

    Update nextcloud_addon.sh

--- tools/nextcloud_addon.sh
+++ tools/nextcloud_addon.sh
@@ -362,11 +362,10 @@ cd /var/www/nextcloud
 sudo -u www-data php occ maintenance:install --database "mysql" --database-name "$databasename"  --database-user "$databaseuser" --database-pass "$databaseuserpasswd" --database-host "localhost:$dbport" --admin-user "$nextroot" --admin-pass "$nextpass" --data-dir "$ncdatafolder"
 sudo -u www-data php occ config:system:set logtimezone --value="$ltz"
 sudo -u www-data php occ config:system:set trusted_domains 1 --value=10.$ipv4network.1
-sudo -u www-data php occ app:enable encryption
-sudo -u www-data php occ encryption:enable
-#fail sudo -u www-data php occ encryption:change-key-storage-root $ncdatafolder/keys
-sudo -u www-data php occ encryption:enable-master-key
-sudo -u www-data php occ encryption:encrypt-all
+#sudo -u www-data php occ app:enable encryption
+#sudo -u www-data php occ encryption:enable
+#sudo -u www-data php occ encryption:enable-master-key
+#sudo -u www-data php occ encryption:encrypt-all
 sudo -u www-data php occ app:enable end_to_end_encryption
 sudo -u www-data php occ background:cron
 
