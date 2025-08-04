74135be1e835a664760fc67202f6fc83d207e93e
keil <38099918+zzzkeil@users.noreply.github.com>
2025-08-04 19:32:43 +0200

    Update remove_client.sh

--- tools/remove_client.sh
+++ tools/remove_client.sh
@@ -7,10 +7,12 @@ echo "."
 echo "."
 ###
 echo "Client Name to remove"
-echo "List Names"
-grep "# Name = " /etc/wireguard/wg0.conf | awk '{print substr($0, 9)}'
+echo "List clients:"
+echo ""
+grep "# Name = " /etc/wireguard/wg0.conf | awk '{print substr($0, 8)}' | tr '\n' ',  '
+echo ""
 echo "Type clientname you want to remove"
-echo "for example: to remove client5 type client5"
+echo "Example: to remove client5 type client5"
 read -p "client name: " -e -i client5 clientname
 echo "------"
 
@@ -20,4 +22,4 @@ rm /etc/wireguard/$clientname.conf
 rm /etc/wireguard/$clientname.png
 sed -i "/# Name = $clientname/,+3 d" /etc/wireguard/wg0.conf
 systemctl restart wg-quick@wg0.service
-echo "Client $clientname removed, wireguard restarted"
+echo "Client $clientname deleted if it existed, wireguard restarted"
