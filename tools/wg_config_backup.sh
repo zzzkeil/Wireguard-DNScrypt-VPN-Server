c214926e2c8ed9f874adfb58fa0826274dc3de06
keil <38099918+zzzkeil@users.noreply.github.com>
2025-08-20 15:44:16 +0200

    Update wg_config_backup.sh

--- tools/wg_config_backup.sh
+++ tools/wg_config_backup.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-clear
-echo " this script backup yours wireguard server config "
-echo " in case if you need to reinstall your server, but not want to reconfigure your all clients"
-echo "."
-echo "."
-echo "."
+if whiptail --title "Backup wireguard config" --yesno "This script backup yours wireguard server config, \nif you need to reinstall your server, but not want to reconfigure your all clients\n\nRun script now ?\n" 15 80; then
+echo ""
+else
+whiptail --title "Aborted" --msgbox "Ok, not now. cu have a nice day." 15 80
+exit 0
+fi   
 tar -cf  /root/backup_wg_config.tar /etc/wireguard
 tar --append --file=/root/backup_wg_config.tar /root/Wireguard-DNScrypt-VPN-Server.README
 tar --append --file=/root/backup_wg_config.tar /etc/dnscrypt-proxy/allowed-names.txt
-echo "."
-echo "."
-echo "."
-echo " done backupfile is /root/backup_wg_config.tar.gz "
-echo " download / copy / save the file to your needs "
-echo " to restore place the file here /root/backup_wg_config.tar.gz and run ./wg_config_restore.sh "
+whiptail --title "Done" --msgbox "Done backupfile is /root/backup_wg_config.tar.gz\nDownload / copy / save the file to your needs\nto restore place the file here /root/backup_wg_config.tar.gz and run ./wg_config_restore.sh" 15 80
+exit 0
