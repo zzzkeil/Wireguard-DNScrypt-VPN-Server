f79cd52360032ec6aebb9a37206ed9a3e5271522
keil <38099918+zzzkeil@users.noreply.github.com>
2025-08-20 15:04:19 +0200

    Update remove_client.sh

--- tools/remove_client.sh
+++ tools/remove_client.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-clear
-echo " To remove a client follow this steps"
-echo "."
-echo "."
-echo "."
-echo "."
-###
-echo "Client Name to remove"
-echo "List clients:"
+if whiptail --title "Remove wireguard client" --yesno "Remove a new wireguard client ?\n" 15 80; then
 echo ""
-grep "# Name = " /etc/wireguard/wg0.conf | awk '{print substr($0, 9)}'
-echo ""
-echo ""
-echo "Type clientname you want to remove"
-echo "Example: to remove client5 type client5"
+else
+whiptail --title "Aborted" --msgbox "Ok, not right now. cu have a nice day." 15 80
+exit 1
+fi  
+
+wgipcheck="/etc/wireguard/wg0.conf"
+client_list=$(grep "# Name = " "$wgipcheck" | awk '{print substr($0, 9)}')
+
+whiptail --title "Remove WireGuard Client" --msgbox "List of existing clients:\n\n$client_list\n\n" 20 60
 while true; do
-    read -p "Client name: " -e -i "$clientname" clientname
-    if [[ -n "$clientname" ]]; then
-        break
+    clientname=$(whiptail --inputbox "Enter the name of the client to remove:" 10 60 "$clientname" 3>&1 1>&2 2>&3)
+    if [ $? -ne 0 ]; then
+        whiptail --msgbox "Cancelled by user." 8 40
+        exit 1
+    fi
+    if [[ -z "$clientname" ]]; then
+        whiptail --msgbox "Client name cannot be empty. Please try again." 8 50
     else
-        echo "Client name cannot be empty. Please enter a valid name."
+        break
     fi
 done
-
-echo "------"
-
 rm /etc/wireguard/keys/$clientname
 rm /etc/wireguard/keys/$clientname.pub
 rm /etc/wireguard/$clientname.conf
