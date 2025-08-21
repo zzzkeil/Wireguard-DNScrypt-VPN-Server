88446740ae9b873277e820ed6fad0e1a643d34c3
keil <38099918+zzzkeil@users.noreply.github.com>
2025-08-21 15:17:29 +0200

    Update nextcloud-behind-wireguard.sh

--- nextcloud-behind-wireguard.sh
+++ nextcloud-behind-wireguard.sh
@@ -152,7 +152,7 @@ install_multiple_packages_with_gauge_ubuntu() {
         done
         echo 100
         echo "All packages installed successfully."
-    } | whiptail --title "Installing needed OS Packages" --gauge "Please wait while installing packages...\napache, php, ...." 15 90 0
+    } | whiptail --title "Installing needed OS Packages" --gauge "Please wait while installing packages...\napache, many php packages, and ...." 15 90 0
 
     if [ $? -eq 0 ]; then
         echo ""
